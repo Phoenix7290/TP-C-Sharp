93f95c069f1ed708250872b3026003f0719a037d
Phoenix7290 <b5261064@gmail.com>
2025-02-12 21:04:50 -0300

    Add project files.

---
+++ TP-C#/Program.cs
@@ -0,0 +1,44 @@
+﻿using System;
+
+class Program
+{
+    static void Main()
+    {
+        // Exercise 5
+        Console.WriteLine("Hello, World!");
+
+        // Exercise 7
+        Console.WriteLine("Algum Erro existiu aqui.");
+        int[] numeros = new int[3];
+        for (int i = 0; i < 3; i++)
+        {
+            numeros[i] = i;
+            Console.WriteLine(numeros[i]);
+        }
+
+        // Exercise 8
+        Console.WriteLine("Qual seu nome?");
+        string? inputName = Console.ReadLine();
+        if (inputName != null)
+        {
+            Console.WriteLine("Olá, " + inputName);
+        }
+        else
+        {
+            Console.WriteLine("Nome não fornecido.");
+        }
+
+        // Exercise 9
+        int age = 18;
+        double height = 1.75;
+        string anotherName = "Rafael";
+        Console.WriteLine("Nome: " + anotherName + ", Idade: " + age + ", Altura: " + height);
+
+        // Exercise 10
+        string nullName;
+        int nullAge;
+        nullName = "Marcos";
+        nullAge = 18;
+        Console.WriteLine("Nome: " + nullName + ", Idade: " + nullAge);
+    }
+}
